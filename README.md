10e61cdc62e792ccb852327bc3ae864ccecfee0f
GuMiner <gus.gran@outlook.com>
2016-11-11 18:12:54 -0800

    Adding the base GuCommon and testing with submodules and Lux

---
+++ README.md
@@ -0,0 +1,19 @@
+# GuCommon
+## Synopsis
+-----------
+Common functionality used throughout my projects to prevent duplication.  
+
+## Description
+--------------
+
+This repository contains common code used throughout my projects. Although a solution is provided to build this code into a 
+DLL for functionality and testing purposes, the author suggests downloading this code as a submodule and integrating the code directly into your application.
+
+## Dependencies
+---------------
+If you're including this DLL or using this code directly, you'll need to also use these dependencies and include their (MIT-compliant) licenses with your source code.
+
+* [GLM 0.9.8.1] (http://glm.g-truc.net/0.9.8/index.html)
+* [GLFW 3.2.1] (http://www.glfw.org/)
+* [GLEW 1.12] (http://glew.sourceforge.net/)
+* [STB latest] (https://github.com/nothings/stb)
---
+++ logging/Logger.h
@@ -0,0 +1,104 @@
+#pragma once
+#include <mutex>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+// A simple class for logging program events out to a file.
+class Logger
+{
+public:
+    enum LogType { INFO, WARN, ERR };
+    static Logger *LogStream;
+
+    // Creates and logs the startup text
+    Logger(const char* fileName);
+
+    // Various convenient logging methods.
+    template<typename T>
+    static void Log(T message)
+    {
+        std::stringstream stringifiedMessage;
+        stringifiedMessage << message;
+        LogStream->LogInternal(LogType::INFO, stringifiedMessage.str().c_str());
+    }
+
+    template<typename T, typename... Remainder>
+    static void Log(T message, Remainder... remainder)
+    {
+        std::string fullMessage = FormFullLogMessage(message, remainder...);
+        LogStream->LogInternal(LogType::INFO, fullMessage.c_str());
+    }
+
+    template<typename T>
+    static void LogWarn(T message)
+    {
+        std::stringstream stringifiedMessage;
+        stringifiedMessage << message;
+        LogStream->LogInternal(LogType::WARN, stringifiedMessage.str().c_str());
+    }
+
+    template<typename T, typename... Remainder>
+    static void LogWarn(T message, Remainder... remainder)
+    {
+        std::string fullMessage = FormFullLogMessage(message, remainder...);
+        LogStream->LogInternal(LogType::WARN, fullMessage.c_str());
+    }
+
+    template<typename T>
+    static void LogError(T message)
+    {
+        std::stringstream stringifiedMessage;
+        stringifiedMessage << message;
+        LogStream->LogInternal(LogType::ERR, stringifiedMessage.str().c_str());
+    }
+
+    template<typename T, typename... Remainder>
+    static void LogError(T message, Remainder... remainder)
+    {
+        std::string fullMessage = FormFullLogMessage(message, remainder...);
+        LogStream->LogInternal(LogType::ERR, fullMessage.c_str());
+    }
+
+    // Control methods for the static Logging instance.
+    static void Setup(std::string logName);
+    static void Shutdown();
+
+    // Destructs the logger
+    ~Logger();
+
+private:
+    // Ensure this is non-copyable my making the copy and assignment operators private.
+    Logger(const Logger&);
+    Logger& operator=(const Logger&);
+
+    std::ofstream logFile;
+    std::mutex writeLock;
+
+    // Recursion finale.
+    template<typename T>
+    static std::string FormFullLogMessage(T message)
+    {
+        std::stringstream streamData;
+        streamData << message;
+        return streamData.str();
+    }
+
+     // Uses variadic templates to form a full log message.
+    template<typename T, typename... Remainder>
+    static std::string FormFullLogMessage(T message, Remainder... remainder)
+    {
+        std::stringstream streamData;
+        streamData << message << Logger::FormFullLogMessage(remainder...);
+        return streamData.str();
+    }
+
+    // Logs a message out the logger
+    void LogInternal(LogType logType, const char* message);
+
+    // Logs the current time out to the log file.
+    void LogTime();
+
+    // Retrieve the log type given the enumeration.
+    const char* GetLogType(LogType logType);
+};
